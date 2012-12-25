a8d2c18cb4065c88c0eb9c6defafb01e46ac6cc4
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2012-12-25 18:00:44 +0100

    Remove old code

--- lib/legit.rb
+++ lib/legit.rb
@@ -1,16 +1,2 @@
-require_relative 'either'
-require_relative 'maybe'
-
 module Legit
-  module Values
-    autoload :Value,        'legit/values/value'
-    autoload :ArrayValue,   'legit/values/array_value'
-    autoload :BooleanValue, 'legit/values/boolean_value'
-    autoload :IntegerValue, 'legit/values/integer_value'
-    autoload :StringValue,  'legit/values/string_value'
-  end
-
-  module Forms
-    autoload :Form, 'legit/forms/form'
-  end
 end
