055afe5eac07290b627b3c7ab8cfb51d7ac1a61e
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2013-04-17 17:08:56 +0200

    Add shared examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,23 @@ require 'minitest/autorun'
 require 'turn'
 
 require 'lego'
+
+
+MiniTest::Spec.class_eval do
+  def self.shared_examples
+    @shared_examples ||= {}
+  end
+end
+
+module MiniTest::Spec::SharedExamples
+  def shared_examples_for(desc, &block)
+    MiniTest::Spec.shared_examples[desc] = block
+  end
+
+  def it_behaves_like(desc)
+    self.instance_eval(&MiniTest::Spec.shared_examples[desc])
+  end
+end
+
+
+Object.class_eval { include(MiniTest::Spec::SharedExamples) }
